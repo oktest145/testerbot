53eee85601f929e8fe6d0dffa59ca720f3f4a6bb
oktest145 <83488344+oktest145@users.noreply.github.com>
2021-07-08 15:41:02 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -23,10 +23,10 @@ The commands I currently support are:
             [
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/HiTechRockets"
+                        "Group", url="https://t.me/ironman_cloud_support"
                     ),
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/HiTechRocket"
+                        "Channel", url="https://t.me/Ironman_Cloud"
                     )
                 ]
             ]
